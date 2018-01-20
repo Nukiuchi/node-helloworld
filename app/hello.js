61900248eb8e8bf1878df2f0ef6bf9a08df121c6
Nukiuchi <mikkosairo@gmail.com>
2018-01-20 20:58:23 +0200

    init

---
+++ app/hello.js
@@ -0,0 +1,7 @@
+// app/hello.js
+
+function sayHello(name) {
+    return "Hello " + name + "!";
+}
+
+module.exports.sayHello = sayHello;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+// app/index.js
+
+var hello = require("./hello");
+
+var name = "world";
+
+var out = hello.sayHello(name);
+
+console.log(out);
