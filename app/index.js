2814fa3d1f4360ec24e9417c023efc6e6034a3bf
Nukiuchi <mikkosairo@gmail.com>
2018-01-21 00:07:47 +0200

    express-handlebars

--- app/index.js
+++ app/index.js
@@ -1,13 +1,20 @@
 // app/index.js
 
+const path = require("path");
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
 const port = 8080;
 
-app.use((request, response, next) => {
-  console.log(request.headers);
-  next();
-});
+app.engine(".hbs", exphbs({
+  defaultLayout: "main",
+  extname: ".hbs",
+  layoutsDir: path.join(__dirname, "views/layouts")
+}));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
+
 
 app.use((request, response, next) => {
   request.chance = Math.random();
@@ -15,8 +22,8 @@ app.use((request, response, next) => {
 });
 
 app.get("/node/hello", (request, response) => {
-  response.json({
-    chance: request.chance
+  response.render("home", {
+    name: "John"
   });
 });
 
